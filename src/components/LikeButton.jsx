274a83e7e1e620664e85390173e3f8c7c04fc2a9
Isaac <idestrosec@gmail.com>
2022-10-10 00:42:36 -0500

    feat(Dashboard): add like button to the movie card

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,31 @@
+import { gql, useMutation } from '@apollo/client'
+import React from 'react'
+
+const UPDATE_LIKES = gql`
+  mutation UpdateLikes($id: String!, $likes: Int!){
+    updateLikes(movieId: $id, likes: $likes)
+  }
+`
+
+function LikeButton({ movieId, likes, refreshNumberLikes }) {
+  const [updateLikesMutation, { loading, data, error }] = useMutation(UPDATE_LIKES, {
+    onCompleted: (data) => {
+      console.log(data);
+      console.log(data.updateLikes);
+    }
+  })
+
+  const handleClick = (id, likes) => {
+    updateLikesMutation({variables: {id, likes: likes+1,}})
+    console.log(data?.updateLikes);
+    refreshNumberLikes(likes+1)
+  }
+
+  return (
+    <button onClick={() => handleClick(movieId, likes)}>
+      Like
+    </button>
+  )
+}
+
+export default LikeButton
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DeleteButton from './DeleteButton'
+import LikeButton from './LikeButton'
 
 function MovieCard({ movie, updateList }) {
+  const [likes, setLikes] = useState(movie.likes);
+
+  const refreshNumberLikes = (newTotal) => {
+    setLikes(newTotal)
+  }
+
   return (
     <div>
       <h3>{movie.title}</h3>
       <p>{movie.id}</p>
       <p>{movie.description}</p>
       <p>{movie.image}</p>
-      <p>{movie.likes}</p>
+      <p>{likes}</p>
 
-      <button type='button'>Like</button>
+      <LikeButton movieId={movie.id} likes={likes} refreshNumberLikes={refreshNumberLikes} />
       <DeleteButton movieId={movie.id} updateList={updateList} />
     </div>
   )
